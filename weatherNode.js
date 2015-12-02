963e29c23214e2f67a2a25413f6e223127356cde
oliverlai55 <oliverlai55@gmail.com>
2015-12-02 14:58:00 -0500

    adding weather api

---
+++ weatherNode.js
@@ -0,0 +1,3 @@
+var http = require('http');
+var apikey = 'e312dbeb8840e51f92334498a261ca1d';
+var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial&APPID="+apikey;
