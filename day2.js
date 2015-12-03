41e14533d0f7a479522ca7a4988815dfe7ec8b8d
oliverlai55 <oliverlai55@gmail.com>
2015-12-03 11:18:21 -0500

    adding the path url with diff ending html after /

--- day2.js
+++ day2.js
@@ -7,5 +7,28 @@ var fs = require('fs');
 var server = http.createServer(function(request, response){
 	//call back is when anytime you have a method that takes the param of 
 	//another method and the method will not run until the method is complete
+	
+	var pathName = url.parse(request.url)
+	if(pathName.pathname == '/'){
+		//user wants to see the home page
+		response.writeHead(200, {
+			'content-type': 'text/html'
+		});
+		response.end('<h1>Home Page</h1>')
+	}else if (pathName.pathname == '/about'){
+		//the user wants to see the about page
+		response.writeHead(200, {
+			'content-type': 'text/html'
+		});
+		response.end('<h1>About Page</h1>')
+	}
 
-})
+	response.writeHead(200, {
+		'content-type': 'text/html'
+	});
+	
+	response.end('<h1>We did it</h1>');
+});
+
+server.listen(3939);
+console.log("The server is listening on port 3939");
--- file-callback.js
+++ file-callback.js
@@ -1,3 +1,22 @@
 console.log('Beginning...');
 console.log('Continuing on...');
 console.log('The End.');
+
+console.log('Beginning...');
+var time = setTimeout(function(){
+	console.log("The End")
+}, 500)
+//even if you put interval as 0, it will still show up as the third line
+//because setTimeout is an event que and will wait for it to finish running
+//before it goes back to printing the event que.
+console.log('continue...');
+
+
+//diff way to code
+console.log('starting'):
+var timer = setTimeout(writeTheEnd, 500);
+console.log('continuing');
+
+function writeTheEnd(){
+	console.log("The End");
+}
